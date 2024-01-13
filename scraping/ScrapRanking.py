c951c6b5763be479918b86ed8c1d7f9bfa8f2dc6
Camille1358 <poupartprin@gmail.com>
2024-01-13 22:56:18 +0100

    request 2 type td data-v-ae1ab4a8=""

--- scraping/ScrapRanking.py
+++ scraping/ScrapRanking.py
@@ -6,8 +6,8 @@ url = 'https://www.shanghairanking.com/rankings/gras/2023/RS0102'
 reponse = requests.get(url)
 if reponse.ok:
     print('rep1 ok')
-    soup = BeautifulSoup(reponse.text)
-    data = soup.find('td data-v-ae1ab4a8 class')
-    print(data)
+    soup = BeautifulSoup(reponse.text, features="lxml")
+    tds = soup. findAll('td data-v-ae1ab4a8=""')
+    [print (str(td) + '\n\n') for td in tds]
 else:
     print('rep1 not ok')
