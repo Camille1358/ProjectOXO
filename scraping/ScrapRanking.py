3be4de142e09b2130e70cf998608e53c476809ce
Camille1358 <poupartprin@gmail.com>
2024-01-13 22:44:34 +0100

    base code data request not working

--- scraping/ScrapRanking.py
+++ scraping/ScrapRanking.py
@@ -1,2 +1,13 @@
 import requests
-import bs4
+from bs4 import BeautifulSoup
+
+url = 'https://www.shanghairanking.com/rankings/gras/2023/RS0102'
+
+reponse = requests.get(url)
+if reponse.ok:
+    print('rep1 ok')
+    soup = BeautifulSoup(reponse.text)
+    data = soup.find('td data-v-ae1ab4a8 class')
+    print(data)
+else:
+    print('rep1 not ok')
