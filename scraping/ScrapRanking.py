cc2c21c9571a326f96352a40cb723fdfff7f9717
Camille1358 <poupartprin@gmail.com>
2024-01-13 22:57:09 +0100

    request obstrued but usable

--- scraping/ScrapRanking.py
+++ scraping/ScrapRanking.py
@@ -6,8 +6,8 @@ url = 'https://www.shanghairanking.com/rankings/gras/2023/RS0102'
 reponse = requests.get(url)
 if reponse.ok:
     print('rep1 ok')
-    soup = BeautifulSoup(reponse.text, features="lxml")
-    tds = soup. findAll('td data-v-ae1ab4a8=""')
+    soup = BeautifulSoup(reponse.text)
+    tds = soup. findAll('td ')
     [print (str(td) + '\n\n') for td in tds]
 else:
     print('rep1 not ok')
